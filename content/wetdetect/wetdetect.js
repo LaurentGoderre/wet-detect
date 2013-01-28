a352e1f871b94b1b3af19e2d6f29848e7a5c7232
Laurent Goderre <laurent.goderre@gmail.com>
2013-01-28 15:44:35 -0500

    JSHinted the javascript code

--- content/wetdetect/wetdetect.js
+++ content/wetdetect/wetdetect.js
@@ -1,6 +1,7 @@
-(function($){
-	wetdetect = {
-		title : "WET Detection",
+(function ($) {
+	'use strict';
+	var wetdetect = {
+		title : 'WET Detection',
 
 		sites_count: 0,
 		sites: null,
@@ -9,138 +10,154 @@
 
 		exec : function () {
 			wetdetect.results = [];
-			$(wetdetect).on("testscomplete", wetdetect._onTestsComplete);
+			$(wetdetect).on('testscomplete', wetdetect._onTestsComplete);
 			wetdetect._loadSites();
 		},
 
-		_loadSites : function(){
+		_loadSites : function( ) {
 			$.ajax({
-				url: "sites.json",
-				dataType:"text",
+				url: 'sites.json',
+				dataType:'text',
 				mimeType: 'text/plain', 
 				statusCode: {
 					0:wetdetect._onSitesLoaded,
 					200:wetdetect._onSitesLoaded
-				 }
-			})
+				}
+			});
 		},
 
-		_processSites : function(){
-			try{
+		_processSites : function () {
+			var site,
+				r;
+
+			try {
 				if (wetdetect.sites.length > 0){
 					site = wetdetect.sites.pop();
-					if (site.url != undefined && site.url.match(/^http/gi)){
-						var r = $.ajax({
+					if (site.url !== undefined && site.url.match(/^http/gi)) {
+						r = $.ajax({
 							type:'GET',
 							url:site.url,
 							context:site,
-							dataType: "html",
-							contentType: "text/html; charset=ISO-8859-1",
+							dataType: 'html',
+							contentType: 'text/html; charset=ISO-8859-1',
 							timeout:20000
 						}).always(wetdetect._onSiteLoaded);
 						if (wetdetect.sites.length > 0){
 							r.always(wetdetect._processSites);
 						}
-					}else{
+					} else {
 						wetdetect._testSite(site, {error:400});
 						if (wetdetect.sites.length > 0){
 							wetdetect._processSites();
 						}
 					}
 				}
-			}catch(e){
-				$("title").text(wetdetect.title + " - An error occured");
+			} catch(e) {
+				$('title').text(wetdetect.title + ' - An error occured');
 			}
 		},
 
-		_testSite : function(site, data){
-			var site = $.extend(site, {r:"UNKNOWN"});
-			if (typeof(data) == "string"){
-				if (data.match(/gcwu-fegc/gi)){
-					site.r = "GCWU/WET";
-				}else if (data.match(/Web Experience Toolkit \(WET\)/gi)){
-					site.r = "WET";
-				}else if (data.match(/PE\.progress/gi) && data.match(/CLF 2.0 TEMPLATE VERSION/gi)){
-					site.r = "CLF2+WET"
-				}else if (data.match(/PE\.progress/gi)){
-					site.r = "WET/NL"
-				}else if (data.match(/CLF 2.0 TEMPLATE VERSION/gi)){
-					site.r = "CLF2"
+		_testSite : function (site, data) {
+			site = $.extend(site, {r:'UNKNOWN'});
+			if (typeof data === 'string'){
+				if (data.match(/gcwu-fegc/gi)) {
+					site.r = 'GCWU/WET';
+				} else if (data.match(/Web Experience Toolkit \(WET\)/gi)) {
+					site.r = 'WET';
+				} else if (data.match(/PE\.progress/gi) && data.match(/CLF 2.0 TEMPLATE VERSION/gi)) {
+					site.r = 'CLF2+WET';
+				} else if (data.match(/PE\.progress/gi)) {
+					site.r = 'WET/NL';
+				} else if (data.match(/CLF 2.0 TEMPLATE VERSION/gi)) {
+					site.r = 'CLF2';
 				}
-			}else{
+			} else {
 				site.r = data.error;
 			}
 			wetdetect.results.push(site);
 		},
 
-		_showResults : function(results){
-			$("body").append("<h2>Results</h2><p id=\"summary\"></p><h2>Dump</h2><table id=\"dump\"><tr><th>Site ID</th><th>URL</th><th>Status</th></tr></table>");
+		_showResults : function () {
+			$('body').append('<h2>Results</h2><p id="summary"></p><h2>Dump</h2><table id="dump"><tr><th>Site ID</th><th>URL</th><th>Status</th></tr></table>');
 
-			var summary = $("#summary");
-			var details = $("#dump");
+			var summary = $('#summary'),
+				details = $('#dump'),
+				r,
+				result,
+				wet=0,
+				gcwuwet=0,
+				clfwet=0,
+				wetnl=0,
+				clf=0,
+				secure=0,
+				notfound=0,
+				error=0,
+				unknown=0;
 
-			var wet=0, gcwuwet=0, clfwet=0, wetnl=0, clf=0, secure=0, notfound=0, error=0, unknown=0;
-			for (r=0;r<wetdetect.results.length; r++){
-				result = wetdetect.results[r]
-				if (result.r == "UNKNOWN"){
+			for (r = 0; r < wetdetect.results.length; r++) {
+				result = wetdetect.results[r];
+				if (result.r === 'UNKNOWN') {
 					unknown++;
-				}else if (result.r == "WET" || result.r == "GCWU/WET"){
+				} else if (result.r === 'WET' || result.r === 'GCWU/WET') {
 					wet++;
-					if (result.r == "GCWU/WET")
+					if (result.r === 'GCWU/WET') {
 						gcwuwet++;
-				}else if (result.r == "CLF2+WET"){
+					}
+				} else if (result.r === 'CLF2+WET') {
 					clfwet++;
-				}else if (result.r == "WET/NL"){
-					wetnl++
-				}else if (result.r == "CLF2"){
-					clf++
-				}else if(result.r == "401" || result.r == "403"){
-					secure++
-				}else if(result.r == "404"){
-					notfound++
-				}else{
+				} else if (result.r === 'WET/NL') {
+					wetnl++;
+				} else if (result.r === 'CLF2') {
+					clf++;
+				} else if(result.r === '401' || result.r === '403') {
+					secure++;
+				} else if(result.r === '404') {
+					notfound++;
+				} else {
 					error++;
 				}
 
-				details.append("<tr><td>" + result.id + "</td><td>" + result.url + "</td><td>" + result.r + "</td></tr>");
+				details.append('<tr><td>' + result.id + '</td><td>' + result.url + '</td><td>' + result.r + '</td></tr>');
 			}
-			summary.text("WET: " + wet + "(GCWU:" + gcwuwet + "), CLF2/WET Hybrid: " + clfwet + ", WET Components (Unknown template): " + wetnl + ", CLF2: " +  clf + ", Secure: " + secure + ", Not Found: " + notfound + ", Other Error: " + error  + ", Unknown: " + unknown);
+			summary.text('WET: ' + wet + '(GCWU:' + gcwuwet + '), CLF2/WET Hybrid: ' + clfwet + ', WET Components (Unknown template): ' + wetnl + ', CLF2: ' +  clf + ', Secure: ' + secure + ', Not Found: ' + notfound + ', Other Error: ' + error  + ', Unknown: ' + unknown);
 		},
 
-		_showProgress : function(){
-			var p = Math.floor(wetdetect.results.length/wetdetect.sites_count*100) + "%";
-			$("#progress").text("Tested " + wetdetect.results.length + " out of " + wetdetect.sites_count + " sites (" + p + ")");
-			try{$("title").text(wetdetect.title + " - " + p);}catch(e){}
+		_showProgress : function () {
+			var p = Math.floor(wetdetect.results.length/wetdetect.sites_count*100) + '%';
+			$('#progress').text('Tested ' + wetdetect.results.length + ' out of ' + wetdetect.sites_count + ' sites (' + p + ')');
+			try{$('title').text(wetdetect.title + ' - ' + p);}catch(e){}
 		},
 
-		_onSitesLoaded : function(response){
-			var jsonText = (typeof response == "string") ? response : response.responseText;
+		_onSitesLoaded : function (response) {
+			var jsonText = (typeof response === 'string') ? response : response.responseText,
+				t;
+
 			wetdetect.sites = $.parseJSON(jsonText);
 			wetdetect.sites_count = wetdetect.sites.length;
-			for (t = 0; t <= wetdetect.concurrent; t++){
+			for (t = 0; t <= wetdetect.concurrent; t++) {
 				wetdetect._processSites();
 			}
 		},
 
-		_onSiteLoaded : function(data){
-			if (typeof(data) == "string"){
+		_onSiteLoaded : function (data) {
+			if (typeof data === 'string'){
 				wetdetect._testSite(this, data);
-			}else{
+			} else {
 				wetdetect._testSite(this, {error: data.status});
 			}
 			wetdetect._showProgress();
 			
-			if (wetdetect.results.length == wetdetect.sites_count){
-				$(wetdetect).trigger("testscomplete");
+			if (wetdetect.results.length === wetdetect.sites_count) {
+				$(wetdetect).trigger('testscomplete');
 			}
 		},
 
-		_onTestsComplete : function(e){
+		_onTestsComplete : function (e) {
 			wetdetect._showResults(e.results);
 		}
-	}
-})(jQuery);
+	};
 
-$(document).ready(function(){
-	wetdetect.exec();
-});
+	$(document).ready(function () {
+		wetdetect.exec();
+	});
+}) (jQuery);
